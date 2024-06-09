54872d2a6a9aa0ea9821ae242e5dbe35e87d727b
Harikrishna2323 <harikb23@gmail.com>
2024-06-09 21:52:24 +0530

    implemented workspace settings page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -2,10 +2,10 @@ import { OrgControl } from "./_components/OrgControl";
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="ml-5">
       <OrgControl />
       {children}
-    </>
+    </div>
   );
 };
 
